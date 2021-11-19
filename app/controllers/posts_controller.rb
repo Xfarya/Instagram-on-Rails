86af05330532442aa64c485c4d3a997378d74eef
Farya <76533997+Xfarya@users.noreply.github.com>
2021-11-19 21:22:27 +0000

    delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,13 @@ def create
   redirect_to root_path
 end
 
+def destroy
+  @post = current_user.posts.find(params[:id])
+  @post.destroy
+  
+  redirect_to user_path(current_user)
+end
+
 private
 def post_params
   params.require(:post).permit(:description, :image, :user_id)
