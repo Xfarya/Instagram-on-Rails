4d0f0b1066f6b2cd375a227269226c4572da27e5
Farya <76533997+Xfarya@users.noreply.github.com>
2021-11-19 21:54:17 +0000

    added hashtag functions

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
 
   resources :posts, only: [:new, :create, :show, :destroy]
+
+  get 'search' => 'search#index'
 end
+
